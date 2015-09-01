1f409b1c98b84d3077b4062c7557dde3e40f5620
Douglas G. Allen <kb9agt@gmail.com>
2015-09-01 08:09:12 -0500

    Update runner.rb

--- runner.rb
+++ runner.rb
@@ -1,17 +1,17 @@
 require "./sinatra.rb"
 
+# Procs instances get assigned to a variable, locally stored block code can be assessed later.
 get "/hi" do
-  puts "I am the hi method"
+  puts "I am in the hi block"
 end
-
-# Proc instance gets assigned to a variable, locally stored block code can be assessed later
-# run("/hi") # Error. before filter is nil
-
-before "/hi" do
-  puts "Running before filter"
-  puts "------------------"
-end
-# same here assigned to a block variable
 
 run("/hi")
+
+before "/hi" do
+  puts "Running before block"
+  puts "------------------"
+end
+
+run("/hi")
+
 run("/random")  #Error
