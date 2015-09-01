445e96f10cedaeb977cf71717055b08427c9767b
Douglas G. Allen <kb9agt@gmail.com>
2015-09-01 07:58:35 -0500

    Update runner.rb

--- runner.rb
+++ runner.rb
@@ -4,14 +4,14 @@ get "/hi" do
   puts "I am the hi method"
 end
 
-#Proc instance gets assigned to a variable, locally stored block code can be assessed later
-
-run("/hi")
+# Proc instance gets assigned to a variable, locally stored block code can be assessed later
+# run("/hi") # Error. before filter is nil
 
 before "/hi" do
   puts "Running before filter"
   puts "------------------"
 end
+# same here assigned to a block variable
 
 run("/hi")
 run("/random")  #Error
