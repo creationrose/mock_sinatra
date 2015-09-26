dddb755bab05ca191a032a8cc80a6a61ce027e5e
Douglas G. Allen <kb9agt@gmail.com>
2015-09-26 06:00:45 -0500

    Update sinatra.rb

--- sinatra.rb
+++ sinatra.rb
@@ -1,9 +1,11 @@
 module Sinatra
 
+  # for the run to message a status
   def self.included(klass)
    puts "Mock Sinatra has been included"
   end
 
+  # calling our procs
   def run(path)    
     if paths.has_key?(path)
       if before_filters.has_key?(path)
@@ -15,19 +17,23 @@ module Sinatra
     end
   end
 
+  # set up the hash or use one
   def before_filters
     @before_filters ||= {}
   end
 
+  # put the proc in a hash
   def before(path, &block)
     puts "Defining a before filter request for #{path}" 
     before_filters[path] = block
   end
 
+  # set the hash or use one
   def paths
     @paths ||= {}
   end
 
+  # put the proc in a hash
   def get(path, &block)
     puts "Defining a GET request for #{path}"    
     paths[path] = block
@@ -35,4 +41,5 @@ module Sinatra
 
 end
 
+# show that sinatra module has been run
 include Sinatra
