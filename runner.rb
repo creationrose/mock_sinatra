629b1d5e51b845af749f41477d7d1a17389eb484
Douglas G. Allen <kb9agt@gmail.com>
2015-09-26 06:29:51 -0500

    Update runner.rb

--- runner.rb
+++ runner.rb
@@ -1,14 +1,17 @@
 require "./sinatra.rb"
 
-# Procs instances get assigned to a variable, locally stored block code can be assessed later.
+# Procs instances get assigned to a hash value, locally stored block code can be assessed later.
 get "/hi" do
-  puts "I am in the hi block"
+  puts "I am the hi proc in a hash."
+  puts "My key is me and my data is a proc."
 end
 
 run("/hi")
 
 before "/hi" do
-  puts "Running before block"
+  puts "I am the before filter proc in a hash."
+  puts "My key is hi and my data is a proc"
+  puts "But I'm a different hash than get is."
   puts "------------------"
 end
 
